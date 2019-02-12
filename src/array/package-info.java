58e3fc65b223f75c79cd23d165fe0803e61642a1
xuyong <363728681@qq.com>
2019-02-12 10:56:07 +0800

    增加包描述文件

---
+++ src/array/package-info.java
@@ -0,0 +1,8 @@
+/**
+ *
+ * leetCode下的数组算法解题记录
+ *
+ * @author xuyong
+ * @since 2019-02-12 10:54
+ **/
+package array;
---
+++ src/random/package-info.java
@@ -0,0 +1,8 @@
+/**
+ *
+ * 按照 leetCode 的算法解题列表顺序解题的记录
+ *
+ * @author xuyong
+ * @since 2019-02-12 10:55
+ **/
+package random;
