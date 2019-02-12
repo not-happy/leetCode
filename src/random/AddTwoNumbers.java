83c1b9e87a33732f315ee5e44769b7d1fbbff334
xuyong <363728681@qq.com>
2019-02-12 14:00:59 +0800

    两数相加

---
+++ src/random/AddTwoNumbers.java
@@ -0,0 +1,43 @@
+package random;
+
+/**
+ * 2. 两数相加 {https://leetcode-cn.com/problems/add-two-numbers/}
+ *
+ * @author xuyong
+ * @since 2019-02-12 10:56
+ **/
+public class AddTwoNumbers {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode(0);
+        ListNode curr = result;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int x = l1 != null ? l1.val : 0;
+            int y = l2 != null ? l2.val : 0;
+
+            int sum = carry + x + y;
+            carry = sum / 10;
+
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return result.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+}
